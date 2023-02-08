852c6d98c6af4d3e096ac6dc1e0807ff0f76a566
Raphaëlla <ninanambinina8@gmail.com>
2023-02-08 11:08:20 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+    function delay() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve("Promise done");
+                }, 5000);
+            });
+        }
+        //console.log(delay());
+
+        delay().then((data) => {
+            console.log(data);
+        })
+        /* 
+                fetch("https://pokeapi.co/api/v2/pokemon").then(data=>{
+                    return data.json();
+                }).then(data=>{
+                    console.log(data);
+                })  */
+
+
+        fetch("https://pokeapi.co/api/v2/pokemon").then(data => {
+            return data.json();
+        }).then(data => {
+
+            for (var i = 0; i < data.results.length; i++) {
+                const $div = document.createElement("div");
+                document.body.append($div)
+                const $link = document.createElement("a");
+                $div.append($link)
+                const show = data.results[i].name
+                console.log(data.results[i].url)
+                $link.textContent = show
+                const idReturn = data.results[i].url.match(/\/(\d+)\/$/)[1]
+                $link.href = "/detail.html?id=" + idReturn
+
+
+            }
+
+            
+        }
+
+
+
+        )
+
+
+
+
+    
